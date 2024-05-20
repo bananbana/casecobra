0bbe886682c2a979ad066491aa26a6e9432126d8
Dawid <97853707+bananbana@users.noreply.github.com>
2024-05-20 17:47:40 +0200

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/dawiddebosz/Documents/casecobra/src/app/layout":["static/media/745fad513dbbaaea-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
